b826d2da84f7e1e3db319c5833fe684df34d7e90
Cyrano webdev <cyrano@cyranowebdev.com>
2021-06-21 12:11:05 -0300

    feat(frontend/admin): remove school

--- front-end/src/components/SchoolCard.js
+++ front-end/src/components/SchoolCard.js
@@ -1,38 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 
 import AppContext from '../context/app.context';
+import api from '../services';
+import { Button } from '.';
 
 export default function SchoolCard({ school, index }) {
-  const { tokenContext: { token } } = useContext(AppContext);
+  const {
+    schoolContext: { setSchools },
+    tokenContext: { token } } = useContext(AppContext);
   const { id, name, address, type, director, status } = school;
 
   const history = useHistory();
 
-  let statusCss = 'status success';
-  if (status === 'Pendente') statusCss = 'status alert';
-  if (status === 'Preparando') statusCss = 'status warning';
+  const schoolTypes = {
+    0: 'Municipal',
+    1: 'Estadual',
+    2: 'Federal',
+  };
 
-  const getOrderDetails = () => {
-    if (token.role === 'admin') return history.push(`/admin/orders/${id}`);
+  const removeSchool = useCallback(async () => {
+    await api.admin.removeSchool(token, id);
+    const refreshSchools = await api.admin.fetchSchools(token);
+    setSchools(refreshSchools);
+  }, [id, token, setSchools]);
+
+  let statusCss = 'status success';
+  if (!status) statusCss = 'status warning';
+
+  const getSchoolDetails = () => {
+    if (token.profile === 'admin') return history.push(`/admin/orders/${id}`);
     history.push(`/orders/${id}`);
   };
 
   return (
     <section
       className="school-card"
-      role="link"
-      onClick={ getOrderDetails }
-      onKeyDown={ getOrderDetails }
+      // role="link"
+      // onClick={ getSchoolDetails }
+      // onKeyDown={ getSchoolDetails }
       tabIndex={ index }
       key={ `${index}-${id}` }
     >
       <section className="name">{ name }</section>
+      <section className="director">{ director }</section>
       <span className="address">{ address }</span>
+      <span className="type">{ schoolTypes[type] }</span>
       <span className={ statusCss }>
         {(status) ? 'Ativa' : 'Sem diretor'}
       </span>
+      <Button
+        label="Deletar"
+        className="status alert"
+        callback={ removeSchool }
+      />
     </section>
   );
 }
--- front-end/src/services/api.admin.js
+++ front-end/src/services/api.admin.js
@@ -102,8 +102,43 @@ const saveSchool = async (token, payload) => {
   }
 };
 
+const removeSchool = async (token, payload) => {
+  try {
+    const headers = { authorization: token.token };
+    const data = { schoolId: payload };
+
+    const request = {
+      method: 'delete',
+      url: `http://${localhost}/admin/school`,
+      headers,
+      data,
+    };
+
+    const result = await axios(request);
+    console.log('Admin remove school: ', data);
+    return result.data;
+  } catch (error) {
+    if (error.response) {
+      // Request made -> server responded with a status code !== 2xx
+      console.log(error.response.data);
+      return error.response.data;
+    }
+    if (error.request) {
+      // Request made -> server NOT responded
+      // `error.request` is an instance of XMLHttpRequest in browser
+      //  and an instance of http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in the setting up of request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  }
+};
+
 export default {
   getDirectors,
   fetchSchools,
   saveSchool,
+  removeSchool,
 };
